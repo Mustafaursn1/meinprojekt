9ae4dc4c46e2f4eca7e10daeee449cd720c825bd
Mustafaursn1 <69422949+Mustafaursn1@users.noreply.github.com>
2025-03-20 22:17:57 +0100

    Teilweise Änderungen

--- src/main/java/com/example/TemperatureService.java
+++ src/main/java/com/example/TemperatureService.java
@@ -1,2 +1,17 @@
-package com.example;public class TemperatureService {
+package com.example;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TemperatureService {
+
+    /**
+     * Berehnet Grad Celsius in Grad Fahrenheit
+     * Siehe auch https://www.fahrenheit-umrechnen.de
+     * @param celsius Grad Celsius
+     * @return Grad Fahrenheit
+     */
+    Double toFahrenheit(Double celsius){
+        return  celsius*1.8d+32;
+    }
 }
